3341074bdb755f14e7a7e828d24b8a86fe69a28c
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-09 10:18:31 -0700

    work on useFetchJobs reducer

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,1 +1,27 @@
-export default function useFetchJobs(params, page) {}
+import { useReducer } from "react";
+
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+    case ACTIONS.GET_DATA:
+    case ACTIONS.ERROR:
+    default:
+      return state;
+  }
+}
+
+export default function useFetchJobs(params, page) {
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
+
+  return {
+    jobs: [],
+    loading: false,
+    error: true
+  };
+}
