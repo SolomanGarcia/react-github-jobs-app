d1f3b88784025b161c0aa63b1aa637879ecca825
Soloman Garcia <soloman.garcia@gmail.com>
2020-11-09 10:50:31 -0700

    create Job component

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 export default function Job({ job }) {
-  return <div>{job.title}</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.title>{job.title}</Card.title>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
